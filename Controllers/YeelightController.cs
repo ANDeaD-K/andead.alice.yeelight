ba7b15d034c3acf956db9705fe5f5f93a58c0b7d
Andrey Kislov <andrej.kislov@spellabs.ru>
2018-12-07 12:26:40 +0300

    Change to test AliceWebhook

--- Controllers/YeelightController.cs
+++ Controllers/YeelightController.cs
@@ -23,14 +23,32 @@ namespace andead.alice.yeelight.Controllers
         [ProducesResponseType(200)]
         [ProducesResponseType(401)]
         // public IActionResult AliceWebhook([FromBody] AliceRequest request)
-        public IActionResult AliceWebhook([FromBody] JObject request)
+        public IActionResult AliceWebhook([FromBody] AliceRequest request)
         // public IActionResult AliceWebhook()
         {
             _logger.LogWarning(JsonConvert.SerializeObject(request));
 
-            string resp = "{\"response\": {\"text\": \"Здравствуйте\", \"end_session\": true}, \"session\": {\"session_id\": \"2eac4854\", \"message_id\": 4, \"user_id\": \"AC9WC3DF6FCE\"}, \"version\": \"1.0\"}";
+            // string resp = "{\"response\": {\"text\": \"Здравствуйте\", \"end_session\": true}, \"session\": {\"session_id\": \"2eac4854\", \"message_id\": 4, \"user_id\": \"AC9WC3DF6FCE\"}, \"version\": \"1.0\"}";
 
-            return Ok(resp);
+            // AliceRequest aliceRequest = JsonConvert.DeserializeObject<AliceRequest>(request);
+
+            AliceResponse response = new AliceResponse()
+            {
+                response = new Response()
+                {
+                    text = "This is test message",
+                    end_session = true
+                },
+                session = new andead.alice.yeelight.Models.Response.Session()
+                {
+                    session_id = request.session.session_id,
+                    message_id = request.session.message_id,
+                    user_id = request.session.user_id
+                },
+                version = "1.0"
+            };
+
+            return Ok(JsonConvert.SerializeObject(response));
         }
 
         [HttpGet("turn-on")]
